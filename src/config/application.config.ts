bc4441a4b582ad627a0c0f9c1f9eb30dd4bf5765
hocle <viethocpro@gmail.com>
2024-08-10 18:57:40 +0700

    Add default value for env

--- src/config/application.config.ts
+++ src/config/application.config.ts
@@ -4,21 +4,21 @@ dotenv.config({ path: '.env' });
 export const ApplicationConfig = {
   serverPort: parseInt(process.env.SERVER_PORT) ?? 8080,
   baseUrl: '/api/v1',
-  loggerDirectory: process.env.LOG_DIR,
+  loggerDirectory: process.env.LOG_DIR ?? './logs',
 
   auth: {
     authHeader: 'x-access-token',
-    tokenSecretKey: process.env.TOKEN_SECRET_KEY,
+    tokenSecretKey: process.env.TOKEN_SECRET_KEY ?? 'mysecretkey',
     accessTokenExpired: parseInt(process.env.ACCESS_TOKEN_EXPIRED_IN_SECOND) ?? 3600, // 1h
     refreshTokenExpired: parseInt(process.env.REFRESH_TOKEN_EXPIRED_IN_SECOND) ?? 604800, // 7d
   },
 
   mail: {
-    activation: process.env.MAIL_ACTIVATION.toLowerCase() === 'true',
-    host: process.env.MAIL_HOST,
-    port: parseInt(process.env.MAIL_PORT),
-    user: process.env.MAIL_USER,
-    pass: process.env.MAIL_PASS,
-    from: process.env.MAIL_FROM,
+    activation: (process.env.MAIL_ACTIVATION ?? '').toLowerCase() === 'true',
+    host: process.env.MAIL_HOST ?? '',
+    port: parseInt(process.env.MAIL_PORT ?? ''),
+    user: process.env.MAIL_USER ?? '',
+    pass: process.env.MAIL_PASS ?? '',
+    from: process.env.MAIL_FROM ?? '',
   },
 };
